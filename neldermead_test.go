e0f303303c2ef7c1e2809ec23254e4a54467343a
Masayuki Muto <masa.edw@gmail.com>
2018-02-05 23:13:09 +0900

    test: Add asset allocation re-balancing

--- neldermead_test.go
+++ neldermead_test.go
@@ -37,3 +37,49 @@ func Test1(t *testing.T) {
 		t.Errorf("fmin too large %f", fmin)
 	}
 }
+
+func Test2(t *testing.T) {
+	// asset allocation re-balancing without selling
+
+	current := []float64{300, 200, 400, 100}
+	targetRatio := []float64{0.25, 0.25, 0.25, 0.25}
+
+	n := 500.0
+	total := 300.0 + 200 + 400 + 100 + n
+	targetNum := make([]float64, len(current))
+	for i := range targetNum {
+		targetNum[i] = total * targetRatio[i]
+		t.Logf("targetNum[%d] = %f\n", i, targetNum[i])
+	}
+
+	f := func(x Vec) float64 {
+		r := 0.0
+		for i := range x {
+			r += math.Pow(targetNum[i]-current[i]-x[i], 2)
+		}
+
+		sum := 0.0
+		for _, v := range x {
+			sum += v
+			if v < 0 {
+				return math.Inf(1)
+			}
+		}
+
+		if n < sum {
+			return math.Inf(1)
+		}
+
+		return r + math.Pow(sum-n, 16)
+	}
+
+	x0 := []float64{0, 0, 100, 0}
+	x, fmin, err := Anms(f, x0)
+	if err != nil {
+		t.Error(err)
+	}
+
+	t.Log(x)
+	t.Log(fmin)
+	t.Log(x[0] + x[1] + x[2] + x[3])
+}
